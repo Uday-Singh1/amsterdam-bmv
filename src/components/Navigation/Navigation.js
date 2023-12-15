6afbf0f11932f4636b13e3937239504325464ec8
Uday Singh <33316@ma-web.nl>
2023-12-15 15:04:42 +0100

    added-content

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navigation.css";
 
 const Navigation = () => {
-  return (
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  
-    <nav className="navigation">
-      <div className="navigation__buttons">
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className={`navigation ${menuOpen ? 'open' : ''}`}>
+      <div className="navigation__image">
+        <img src="./img/amsterdam-logo.png" alt="Amsterdam" />
+      </div>
+      <div className={`navigation__buttons ${menuOpen ? 'open' : ''}`}>
         <button className="navigation__button">Projecten</button>
         <button className="navigation__button">Expertise</button>
         <button className="navigation__button">Mensen</button>
         <button className="navigation__button">Publicaties</button>
         <button className="navigation__button">Contact</button>
       </div>
-      <div className="navigation__image">
-        <img src="./img/amsterdam-xxx.svg" alt="Amsterdam" />
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
 };
 
 export default Navigation;
-
